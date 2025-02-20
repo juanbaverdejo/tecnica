c92fb00a6eeb9c0ec81b0ccc84f6c2e9c7a96ad4
DIEC6 <juanbaverdejo@gmail.com>
2025-02-20 10:29:31 -0300

    cargaautomatica

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using PruebaTecnica.datos;
+using PruebaTecnica.Migrations;
+using PruebaTecnica.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,4 +33,32 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Tarjeta}/{action=Home}/{id?}");
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    await dbContext.Database.MigrateAsync(); // Aplica cualquier migración pendiente
+    await ApplicationDbContextSeed.SeedAsync(dbContext); // Insertar datos de ejemplo si es necesario
+}
 app.Run();
+public class ApplicationDbContextSeed
+{
+    public static async Task SeedAsync(ApplicationDbContext context)
+    {
+        DateTime fechaEspecifica = new DateTime(2027, 12, 30); // 30 de diciembre de 2027
+
+        // Si no hay productos, agregar algunos datos iniciales
+        if (!context.Tarjeta.Any())
+        {
+            var products = new List<Tarjeta>
+            {
+                new Tarjeta {NumeroTarjeta = "1111111111111111", PIN = "1111", Balance=10000,Bloqueada=false, IntentosFallidos=0,FechaVencimiento=fechaEspecifica},
+                new Tarjeta {NumeroTarjeta = "1111222233334444", PIN = "1234", Balance=10000,Bloqueada=false, IntentosFallidos=0,FechaVencimiento=fechaEspecifica},
+            };
+
+            await context.Tarjeta.AddRangeAsync(products);
+            await context.SaveChangesAsync();
+        }
+    }
+}
+
+
